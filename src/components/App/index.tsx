5c9672f59eba99069c221e10beb0952699b34ad0
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2022-09-19 22:09:55 +0500

    added mui5

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,38 +1,35 @@
-import React, { FC } from "react";
-import { SupportedWallets } from "../../connection";
+import { FC } from "react";
 import { Ember, Erc20, Kolnet } from "@contracts/types";
 import { useWeb3React } from "@web3-react/core";
-import { AvailableContracts } from "../../hooks/useContract/types";
-import { useContract } from "../../hooks/useContract";
-import WalletButtons from "@components/WalletButtons";
-import { ConnectWallet } from "@thirdweb-dev/react";
-
-import "./styles.scss";
+import { AvailableContracts } from "hooks/useContract/types";
+import { useContract } from "hooks/useContract";
+// import WalletButtons from "@components/WalletButtons";
+import ConnectWallet from "@components/ConnectWallet";
 
 type Props = Record<string, unknown>;
 
 const App: FC<Props> = () => {
   const { provider, account } = useWeb3React();
-  const tusdtContract = useContract<Erc20>(AvailableContracts.TUSDT);
-  const kolnetContract = useContract<Kolnet>(AvailableContracts.KOLNET);
-  const emberContract = useContract<Ember>(AvailableContracts.EMBER);
+  // const tusdtContract = useContract<Erc20>(AvailableContracts.TUSDT);
+  // const kolnetContract = useContract<Kolnet>(AvailableContracts.KOLNET);
+  // const emberContract = useContract<Ember>(AvailableContracts.EMBER);
 
   console.log("account -", account, provider);
 
-  const tryTransfer = async () => {
-    if (tusdtContract) {
-      tusdtContract.transfer(
-        "0x09050568Ed00123dA7d9250c8A57AD393EeD8307",
-        "1000000"
-      );
-    }
-  };
+  // const tryTransfer = async () => {
+  //   if (tusdtContract) {
+  //     tusdtContract.transfer(
+  //       "0x09050568Ed00123dA7d9250c8A57AD393EeD8307",
+  //       "1000000"
+  //     );
+  //   }
+  // };
 
   return (
     <div className="app_container">
-      <WalletButtons wallets={[SupportedWallets.METAMASK]} />
       <ConnectWallet />
-      <button onClick={tryTransfer}>send</button>
+      {/* <WalletButtons wallets={[SupportedWallets.METAMASK]} /> */}
+      {/* <button onClick={tryTransfer}>send</button> */}
     </div>
   );
 };
---
+++ src/components/ConnectWallet/index.tsx
@@ -0,0 +1,11 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+
+const ConnectWalletPlugin = () => {
+  return (
+    <div>
+      <ConnectWallet />
+    </div>
+  );
+};
+
+export default ConnectWalletPlugin;
