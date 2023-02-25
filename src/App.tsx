cbe1ba515b0745587bee02529bde25fba5729044
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2023-02-26 02:06:15 +0500

    feat: wagmi setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { WagmiConfig, createClient } from "wagmi";
+import { getDefaultProvider } from "ethers";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Profile from "./components/Profile";
 
+const client = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <WagmiConfig client={client}>
+      <Profile />
+    </WagmiConfig>
+  );
+};
 
-export default App
+export default App;
