d49384ba4d581ee2a09e3ba56cc6275eaa49c0d2
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2023-02-26 02:53:01 +0500

    feat: mui setup

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,31 +1,41 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+
+import { Button } from "@mui/material";
 
 export function Connect() {
-  const { connector, isConnected } = useAccount()
+  const { connector, isConnected } = useAccount();
   const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect()
-  const { disconnect } = useDisconnect()
+    useConnect();
+  const { disconnect } = useDisconnect();
 
   return (
     <div>
       <div>
         {isConnected && (
-          <button onClick={() => disconnect()}>
+          <Button
+            variant="outlined"
+            color="error"
+            onClick={() => disconnect()}
+          >
             Disconnect from {connector?.name}
-          </button>
+          </Button>
         )}
 
         {connectors
           .filter((x) => x.ready && x.id !== connector?.id)
           .map((x) => (
-            <button key={x.id} onClick={() => connect({ connector: x })}>
+            <Button
+              variant="outlined"
+              key={x.id}
+              onClick={() => connect({ connector: x })}
+            >
               {x.name}
-              {isLoading && x.id === pendingConnector?.id && ' (connecting)'}
-            </button>
+              {isLoading && x.id === pendingConnector?.id && " (connecting)"}
+            </Button>
           ))}
       </div>
 
       {error && <div>{error.message}</div>}
     </div>
-  )
+  );
 }
